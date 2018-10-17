a79cd175478236e1749fcd8b4ce8ac1183ee9734
CE <archmagece@users.noreply.github.com>
2018-10-17 19:56:45 +0900

    Create update_git_all.sh

---
+++ update_git_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# store the current dir
+CUR_DIR=$(pwd)
+
+# Let the person running the script know what's going on.
+echo "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
+
+# Find all git repositories and update it to the master latest revision
+#for i in $(find . -maxdepth 3 -type d -name ".git" | cut -c 3-); do
+for i in $(find . -maxdepth 3 -type d -name ".git"); do
+    echo "";
+    echo "\033[33m"+$i+"\033[0m";
+
+    # We have to go to the .git parent directory to call the pull command
+    cd "$i";
+    cd ..;
+
+    # finally pull
+    #git reset --hard HEAD
+    #git pull origin master;
+    git fetch origin master;
+    git reset --hard origin/master;
+
+    # lets get back to the CUR_DIR
+    cd $CUR_DIR
+done
+
+echo "\n\033[32mComplete!\033[0m\n"
